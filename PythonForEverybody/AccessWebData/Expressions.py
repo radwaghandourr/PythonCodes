1137250352f41e3c89e9f728063aca3e8ad7646f
GHANDOUR Radwa OBS/GDO <mostafaradwa@gmail.com>
2024-03-30 17:42:01 +0200

    Loading page

--- PythonForEverybody/AccessWebData/Expressions.py
+++ PythonForEverybody/AccessWebData/Expressions.py
@@ -1,13 +1,16 @@
 import re
-text= "I like to try new things from the internet"
+text= "I like to try new things: I find in the internet"
 for word in text:
     #word=word.rstrip()
     #""Object Oriented Pattern where we take variable name.method/ library name.function""
     #The if statement here returns true or false
-    if re.search('I',word):
-        print(word)
+    #"Adding dot then * means followed by any number of words till word 'new' even if there is a space between them "
+    x=re.search("^I.*new", text)
+    print(x)
     #The is the same function as the regural expression
-    if word.find('I')>=0:
-        print (word)
+    #if word.find('I')>=0:
+     #   print (word)
+
+
 
 
